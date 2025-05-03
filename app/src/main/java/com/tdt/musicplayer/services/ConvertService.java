52df15c0367a4f8f9d097b3e9375f81d155c145a
Dang Thai Tay <dangthaitay96@gmail.com>
2025-05-03 22:51:51 +0700

    .

--- app/src/main/java/com/tdt/musicplayer/services/ConvertService.java
+++ app/src/main/java/com/tdt/musicplayer/services/ConvertService.java
@@ -1,35 +1,21 @@
 package com.tdt.musicplayer.services;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.media.MediaScannerConnection;
-import android.os.Environment;
-import android.widget.Toast;
-
 import com.arthenica.ffmpegkit.FFmpegKit;
 import com.arthenica.ffmpegkit.ReturnCode;
 import com.arthenica.ffmpegkit.Session;
-
-import org.schabi.newpipe.extractor.NewPipe;
-import org.schabi.newpipe.extractor.exceptions.ExtractionException;
-import org.schabi.newpipe.extractor.stream.AudioStream;
-import org.schabi.newpipe.extractor.stream.StreamInfo;
-
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 public class ConvertService {
 
   public File convertToMp3(File m4aFile, File outputDir, String songName) {
-    String mp3Path = new File(outputDir, songName + System.currentTimeMillis() + ".mp3").getAbsolutePath();
-    String command = "-i \"" + m4aFile.getAbsolutePath() + "\" -vn -ar 44100 -ac 2 -b:a 192k \"" + mp3Path + "\"";
+    String mp3Path =
+        new File(outputDir, songName + System.currentTimeMillis() + ".mp3").getAbsolutePath();
+    String command =
+        "-i \""
+            + m4aFile.getAbsolutePath()
+            + "\" -vn -ar 44100 -ac 2 -b:a 192k \""
+            + mp3Path
+            + "\"";
 
     Session session = FFmpegKit.execute(command);
     if (ReturnCode.isSuccess(session.getReturnCode())) {
